056718a9cf7092fa79a6948b250c35ba33d0c424
tejaswibhagat <107565043+tejaswibhagat@users.noreply.github.com>
2023-06-20 19:00:56 +0530

    INTRO CODING Creating a button and wraping it inside the surface to bring elevation and aligning it to the center

--- app/src/main/java/com/example/projectudemyintro/MainActivity.kt
+++ app/src/main/java/com/example/projectudemyintro/MainActivity.kt
@@ -13,13 +13,16 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projectudemyintro.ui.theme.ProjectUdemyIntroTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,42 +39,57 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(){
-    Surface(
-        modifier = Modifier.fillMaxSize()
-        ,
-        color = Color(0xFF5446E7A)
-    ){
 
+    Surface(modifier = Modifier
+        .fillMaxSize(),
+        color = Color(0xFF5446E7A) ){
+        //Text(text = "Hello")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "$ 100", style = TextStyle(
+                color = Color.White,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            CreateCircle()
+        }
     }
 }
 
 @Preview
 @Composable
-fun Circle(){
-    Card(
-        modifier = Modifier
-            .padding(3.dp)
-            .size(105.dp)
-            .clickable {
-                Log.d("Tap", "Circle: Tap")
-            },
+fun CreateCircle(){
+    var moneyCounter by remember{
+        mutableStateOf(0)
+    }
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(105.dp)
+        .clickable {
+            moneyCounter += 1
+            Log.d("Counter", "CreateCircle: $moneyCounter")
+        },
         shape = CircleShape,
         elevation = 4.dp
-        ) {
-        Box(contentAlignment = Alignment.Center) {
-            Text(text = "TAP", modifier = Modifier)
+    ) {
+        Box(contentAlignment = Alignment.Center){
+            Text(text = "Tap $moneyCounter")
         }
-
     }
 }
 
 
-@Preview(showBackground = true)
+@Preview(  showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ProjectUdemyIntroTheme {
-
-     MyApp()
+    ProjectUdemyIntroTheme() {
+        MyApp()
 
     }
 }
+
